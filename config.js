9e3920fc49617c68171da627a14d98de72b1e2c2
MJ <kipsang20323@gmail.com>
2018-04-17 21:16:40 -0600

    travis push

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-exports.DATABASE_URL = 'mongodb://outdoorsman:deere45678@ds247619.mlab.com:47619/outdoors-app';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/test-outdoors-app';
 //pre mLab connection: 'mongodb://localhost/outdoors-app';
-exports.TEST_DATABASE_URL = 'mongodb://localhost/test-outdoors-app';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-outdoors-app';
 exports.PORT = process.env.PORT || 8080;
-exports.JWT_SECRET = process.env.JWT_SECRET;
+exports.JWT_SECRET = process.env.JWT_SECRET || 'secret';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
