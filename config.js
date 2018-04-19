11a96be52dacda11f97732a93b5e5eea1da51afb
MJ <kipsang20323@gmail.com>
2018-04-18 19:54:41 -0600

    final DB string swap

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://outdoorsman:deere45678@ds247619.mlab.com:47619/outdoors-app';
+// exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://outdoorsman:deere45678@ds247619.mlab.com:47619/outdoors-app';
 // 'mongodb://outdoorsman:deere45678@ds247619.mlab.com:47619/outdoors-app';
-// exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/outdoors-app';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/outdoors-app';
 //pre mLab connection: 'mongodb://localhost/outdoors-app';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-outdoors-app';
 //mongodb://testingapp:tester1234@ds249249.mlab.com:49249/test-outdoors-app
