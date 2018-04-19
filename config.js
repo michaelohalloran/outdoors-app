7c57eebc154b2aa2bef661aba7f9f1c2ce319bd8
MJ <kipsang20323@gmail.com>
2018-04-18 19:52:43 -0600

    add new testDB mLab string

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://outdoorsman:deere4
 // exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/outdoors-app';
 //pre mLab connection: 'mongodb://localhost/outdoors-app';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-outdoors-app';
+//mongodb://testingapp:tester1234@ds249249.mlab.com:49249/test-outdoors-app
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET || 'secret';
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
